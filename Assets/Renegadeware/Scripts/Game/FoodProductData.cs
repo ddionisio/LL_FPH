c39f3ac4e1a47f3a0835f622c12d6146302b9ea5
David Dionisio <ddionisio@renegadeware.com>
2020-02-26 18:08:20 -0500

    food products art assets, prep. food product data

---
+++ Assets/Renegadeware/Scripts/Game/FoodProductData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Renegadeware {
+    [CreateAssetMenu(fileName = "foodProduct", menuName = "Game/Food Product")]
+    public class FoodProductData : ScriptableObject {
+        public Sprite iconSprite;
+
+        [M8.Localize]
+        public string nameRef;
+
+        public bool isHazardous;
+    }
+}
