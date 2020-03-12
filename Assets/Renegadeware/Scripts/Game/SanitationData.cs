4f0b17f05df308aeb4844b9422fc4336929160c1
David Dionisio <ddionisio@renegadeware.com>
2020-03-11 20:59:35 -0400

    sanitation stuff

---
+++ Assets/Renegadeware/Scripts/Game/SanitationData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Renegadeware {
+    [CreateAssetMenu(fileName = "sanitationData", menuName = "Game/Sanitation Data")]
+    public class SanitationData : ScriptableObject {
+        public int score;
+    }
+}
